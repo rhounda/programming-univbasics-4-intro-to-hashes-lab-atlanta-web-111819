6b4ac21fd113ce3c72fcd7cd785c4c2909652f48
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-11 03:44:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,30 @@
 def new_hash
-  # return an empty hash
+new_hash = hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
-end
+my_hash = {
+  'gender' => 'male'
+}
+  end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pinoneer = {
+    :name => 'GraceHopper'
+  }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    :id => 3
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+ my_hash_creator = {
+   :key => 'value'
+   
+ }
 end
 
 def read_from_hash(hash, key)
